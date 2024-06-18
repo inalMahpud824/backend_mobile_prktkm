e857c555becf8e5ae83fbe5b322e4d5da6e0fc68
Inal Mahpud <inalmahpud1@gmail.com>
2024-06-18 15:50:26 +0700

    first commit

---
+++ src/application/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import { errorHandler } from "../middleware/error.js";
+import { router } from "../routes/api.js";
+export const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+app.use(router);
+app.use(errorHandler);
+
---
+++ src/controllers/auth.js
@@ -0,0 +1,19 @@
+import { login, register } from "../services/auth.js";
+export const authController = {
+  register: async (req, res, next) => {
+    try {
+      const result = await register(req.body);
+      return res.status(201).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
+  login: async (req, res, next) => {
+    try {
+      const result = await login(req.body);
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
